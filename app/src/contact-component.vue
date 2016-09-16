fd7d228a8d2702a2fc721827a8ca9beaf7d7abca
Gabriel SULLICE <gabriel@sullice.com>
2016-09-16 09:43:24 -0600

    Enable PATCHing contacts!

--- app/src/contact-component.vue
+++ app/src/contact-component.vue
@@ -12,20 +12,24 @@
     <span class="glyphicon glyphicon-pencil edit-icon" aria-hidden="true"></span>
   </div>
   <div class="editor" v-if="editOpen">
-    <form class="form-horizontal">
-      <div class="form-group col-md-6 col-offset-md-3">
+    <form class="edit-form form-horizontal">
+      <div class="form-group">
         <label for="#first-name">First Name</label>
-        <input id="first-name" type="text" class="form-control">
+        <input id="first-name" type="text" class="form-control" v-model="contact.attributes.firstName">
       </div>
-      <div class="form-group col-md-6 col-offset-md-3">
+      <div class="form-group">
         <label for="#last-name">Last Name</label>
-        <input id="last-name" type="text" class="form-control">
+        <input id="last-name" type="text" class="form-control" v-model="contact.attributes.lastName">
       </div>
-      <div class="form-group col-md-6 col-offset-md-3">
+      <div class="form-group">
         <label for="#email">Email</label>
-        <input id="email" type="email" class="form-control">
+        <input id="email" type="email" class="form-control" v-model="contact.attributes.emails[0]">
       </div>
-      <input id="submit" type="submit" value="Save" class="btn btn-success">
+      <div class="form-group">
+        <label for="#phone">Phone</label>
+        <input id="phone" type="tel" class="form-control" v-model="contact.attributes.phones[0].number">
+      </div>
+      <button id="submit" type="button" class="btn btn-success" v-on:click="updateContact()">Save</button>
     </form>
   </div>
 </div>
@@ -42,6 +46,26 @@ export default {
   methods: {
     toggleEdit () {
       this.editOpen = !this.editOpen
+    },
+    updateContact () {
+      const body = {
+        "data": {
+          "id": this.contact.id,
+          "type": "contacts",
+          "attributes": {
+            "firstName": this.contact.attributes.firstName,
+            "lastName": this.contact.attributes.lastName,
+            "emails": this.contact.attributes.emails,
+            "phones": this.contact.attributes.phones
+          }
+        }
+      }
+
+      this.$http.patch("http://contacts.atendesigngroup.com/v0/contacts/" + this.contact.id, body, (resp) => {
+        console.log("Success!")
+      }, (resp, status, req) => {
+        console.log(resp)
+      })
     }
   }
 }
