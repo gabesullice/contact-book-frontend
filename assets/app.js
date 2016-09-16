b386347062e5d82ae48224259d11fe606813887f
Gabriel SULLICE <gabriel@sullice.com>
2016-09-15 22:03:31 -0600

    Add fancy edit icon

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,15 @@ var Contact = Vue.extend({
   props: ['contact'],
   template: `
 <div class="inline-block">
-<p class="name"><span class="lead">{{ contact.attributes.firstName }} {{ contact.attributes.lastName }}</span></p>
-<p class="organization">{{ contact.attributes.name }}</p>
+  <p class="name"><span class="lead">{{ contact.attributes.firstName }} {{ contact.attributes.lastName }}</span></p>
+  <p class="organization">{{ contact.attributes.name }}</p>
 </div>
 <div class="inline-block pull-right">
-<p class="email"><a href="mailto:{{ contact.attributes.emails[0] }}">{{ contact.attributes.emails[0] }}</a></p>
-<p class="phone"><a href="tel:{{ contact.attributes.phones[0].number }}">{{ contact.attributes.phones[0].number }}</a></p>
+  <p class="email"><a href="mailto:{{ contact.attributes.emails[0] }}">{{ contact.attributes.emails[0] }}</a></p>
+  <p class="phone"><a href="tel:{{ contact.attributes.phones[0].number }}">{{ contact.attributes.phones[0].number }}</a></p>
+</div>
+<div class="pencil-container">
+  <span class="glyphicon glyphicon-pencil edit-icon" aria-hidden="true"></span>
 </div>
 `
 });
